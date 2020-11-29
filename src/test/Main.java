6a76c2f7c0ce6dcde29989d00a3e806e6544feaa
Wojciech Kosztyła <305338@student.agh.edu.pl>
2020-11-29 20:39:57 +0100

    Test init

---
+++ src/test/Main.java
@@ -0,0 +1,11 @@
+package test;
+
+import java.util.LinkedList;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
