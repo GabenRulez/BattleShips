11122bcb5fce63bc2a830cd3e060099ab574542f
GabenRulez <45923220+GabenRulez@users.noreply.github.com>
2021-01-20 15:26:02 +0100

    Added a class that will be responsible of console output, so called "beautifier".

---
+++ src/main/java/gabenrulez/helper/texter.java
@@ -0,0 +1,103 @@
+package gabenrulez.helper;
+
+import java.util.HashMap;
+
+public class texter {
+    public static final int consoleWidth = 80;
+    private static final int tabWidth = 4;
+    public static HashMap<Character, String> previousLines = new HashMap<>();
+
+    public static String createCharLine(char character, int amount){
+        String temp = String.valueOf(character);
+        int yetToWrite = amount - 1;
+        int alreadyWritten = 1;
+        while(yetToWrite > 0){
+            if(yetToWrite > alreadyWritten){
+                temp += temp;
+                yetToWrite -= alreadyWritten;
+                alreadyWritten *= 2;
+            }
+            else{
+                String temp2 = texter.createCharLine(character, yetToWrite);
+                temp += temp2;
+                alreadyWritten += yetToWrite;
+                yetToWrite = 0;
+            }
+        }
+        return temp;
+    }
+
+    public static String createCharLine(char character){
+        String result = previousLines.get(character);
+        if(result == null){
+            result = createCharLine(character, texter.consoleWidth);
+            previousLines.put(character, result);
+        }
+        return result;
+    }
+
+    public static String createCentered(String text){
+        if(text.length() > texter.consoleWidth) return text;
+        return createCharLine(' ', (texter.consoleWidth - text.length())/2) + text;
+    }
+
+    public static String createTabbed(String text, int amountOfTabs){
+        return createCharLine(' ', texter.tabWidth * amountOfTabs) + text;
+    }
+
+    public static String createTabbed(String text){
+        return createTabbed(text, 1);
+    }
+
+    public static String createNewLines(int amount){
+        String temp = "\n";
+        int yetToWrite = amount - 1;
+        int alreadyWritten = 1;
+        while(yetToWrite > 0){
+            if(yetToWrite > alreadyWritten){
+                temp += temp;
+                yetToWrite -= alreadyWritten;
+                alreadyWritten *= 2;
+            }
+            else{
+                String temp2 = texter.createNewLines(yetToWrite);
+                temp += temp2;
+                alreadyWritten += yetToWrite;
+                yetToWrite = 0;
+            }
+        }
+        return temp;
+    }
+
+    public static String createNewLine(){
+        return createNewLines(1);
+    }
+
+    public static void printCharLine(char character){
+        System.out.println(createCharLine(character));
+    }
+
+    public static void printCentered(String text){
+        System.out.println(createCentered(text));
+    }
+
+    public static void printTabbed(String text){
+        System.out.println(createTabbed(text));
+    }
+
+    public static void printNewLines(int amount){
+        System.out.print(createNewLines(amount));
+    }
+
+    public static void printNewLine(){
+        System.out.print(createNewLine());
+    }
+
+    public static void printMessage(String messageText){
+        System.out.println(createNewLine() + createCharLine('#') + createNewLine() + createCentered(messageText) + createNewLine() + createCharLine('#') + createNewLine());
+    }
+
+    public static void printErrorMessage(String errorSource, String errorMessage){
+        System.out.println(createNewLine() + createCharLine('*') + createNewLine() + createTabbed("Error from '" + errorSource + "': " + errorMessage) + createNewLine() + createCharLine('*') + createNewLine());
+    }
+}
